d7742419c381d6250cb7ed041b3703535e449655
James Reinhardt <liverat@student.ubc.ca>
2024-04-27 12:33:25 -0700

    Updated tests

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+//! src/lib.rs
+use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::dev::Server;
+use std::net::TcpListener;
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+// Let's start simple: we always return a 200 OK
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+// Notice the different signature!
+// We return `Server` on the happy path and we dropped the `async` keyword
+// We have no .await call, so it is not needed anymore.
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            // A new entry in our routing table for POST /subscriptions requests
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+        .listen(listener)?
+        .run();
+    Ok(server)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+//! src/main.rs
+use std::net::TcpListener;
+use zero2prod::run;
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+// Bubble up the io::Error if we failed to bind the address
+// Otherwise call .await on our Server
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    run(listener)?.await
 }
